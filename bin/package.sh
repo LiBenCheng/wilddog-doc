23a345c8c940766228cee5292f1825f237cfd276
libei <libei@wilddog.com>
2016-03-15 14:29:10 +0800

    commit

--- bin/package.sh
+++ bin/package.sh
@@ -6,6 +6,7 @@ PRGDIR=`cd $PRGDIR  >/dev/null; pwd `
 rm -rf dist
 mkdir dist
 
+chmod a+x node_modules -R
 gulp build
 cd dist
 tar cvfz wilddog-doc.tar.gz *  --exclude=".*" --exclude=node_modules .
